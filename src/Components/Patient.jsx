ddecbb47a1bea80a704755fd4f37f362682c73b2
Soumik Acharjee <acharjee266@gmail.com>
2020-03-29 22:40:45 +0530

    changes

--- src/Components/Patient.jsx
+++ src/Components/Patient.jsx
@@ -19,59 +19,76 @@ function Patient() {
   }, []);
   const nextPageHandler = () => {
     let temp = totalData;
-    temp = temp.slice((page + 1) * 5, (page + 1) * 5 + 5);
+    temp = temp.slice((page + 1) * 10, (page + 1) * 10 + 10);
     setPerData(temp, page);
     setPage(page + 1);
   };
   const prevPagehandler = () => {
     let temp = totalData;
     if (page == 1) {
-      temp = temp.splice(0, 5);
+      temp = temp.splice(0, 10);
     } else {
-      temp = temp.slice((page - 1) * 5, (page - 1) * 5 + 5);
+      temp = temp.slice((page - 10) * 10, (page - 1) * 10 + 10);
     }
     setPerData(temp);
     setPage(page - 1);
   };
-  console.log(perData);
   return (
-    <div>
+    <div className="text-center">
       <h2>Total Patients Admitted {totalPatient}</h2>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">reported On</th>
-            <th scope="col">age</th>
-            <th scope="col">gender</th>
-            <th scope="col">city</th>
-            <th scope="col">state</th>
-            <th scope="col">status</th>
-            <th scope="col">sources</th>
-            <th scope="col">note</th>
-          </tr>
-        </thead>
-        <tbody>
-          {perData.map(ele => {
-            return (
-              <tr key={ele.patientId}>
-                <th scope="row">{ele.patientID}</th>
-                <td scope="row">{ele.reportedOn}</td>
-                <td scope="row">{ele.ageEstimate}</td>
-                <td scope="row">{ele.gender}</td>
-                <td scope="row">{ele.city}</td>
-                <td scope="row">{ele.state}</td>
-                <td scope="row">{ele.status}</td>
-                <td scope="row">{ele.sources}</td>
-                <td scope="row">{ele.notes}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      {page > 0 ? <button onClick={prevPagehandler}>Prev</button> : null}
+      <div className="table-responsive">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">date</th>
+              <th scope="col">age</th>
+              <th scope="col">gender</th>
+              <th scope="col">city</th>
+              <th scope="col">state</th>
+              <th scope="col">status</th>
+              <th scope="col">sources</th>
+              <th scope="col">note</th>
+            </tr>
+          </thead>
+          <tbody>
+            {perData.map(ele => {
+              return (
+                <tr key={ele.patientId}>
+                  <th scope="row">{ele.patientId}</th>
+                  <td scope="row">{ele.reportedOn}</td>
+                  <td scope="row">{ele.ageEstimate}</td>
+                  <td scope="row">{ele.gender}</td>
+                  <td scope="row">{ele.city}</td>
+                  <td scope="row">{ele.state}</td>
+                  <td scope="row">{ele.status}</td>
+                  <td scope="row">
+                    <a target="_blank" href={ele.sources}>
+                      source
+                    </a>
+                  </td>
+                  <td scope="row">{ele.notes}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      {page > 0 ? (
+        <button
+          className="badge m-4 badge-pill badge-danger"
+          onClick={prevPagehandler}
+        >
+          Prev
+        </button>
+      ) : null}
       {perData.length > 0 ? (
-        <button onClick={nextPageHandler}>Next</button>
+        <button
+          className="badge m-4 badge-pill badge-danger"
+          onClick={nextPageHandler}
+        >
+          Next
+        </button>
       ) : null}
     </div>
   );
